5c8809ef0eaffa51c911e7287485e4904518aed4
Антон Румянцев <96540911+RedmanPlus@users.noreply.github.com>
2022-06-09 20:38:38 +0300

    Update views.py

--- django/api/views.py
+++ django/api/views.py
@@ -15,11 +15,15 @@ class VacanciesViewSet(viewsets.ModelViewSet):
         data = request.data
 
         # Ниже хендлим все поля ForeignKey/Many-to-Many, т.е. с зависимостями от других таблиц
-
         # роли нельзя добавить POST-ом, поэтому при отсутствии роли в БД поднимается Exception
         # С ошибкой 500. Нужен try/except с raise Role.DoesNotExists.
         role_obj = Role.objects.filter(**data['role']).first()
         data['role'] = role_obj
+
+        # канал нельзя создать POST-ом, поэтому при отсутсвии канала в БД поднимается Exception
+        # С ошибкой 500. Нужен try/except с raise Channel.DoesNotExists
+        channel_obj = Channel.objects.filter(**data['channel_id']).first()
+        data['channel_id'] = channel_obj
 
         # ищем локацию в БД, если её нет - создаём
         location_obj = Location.objects.filter(**data['location']).first()
@@ -27,11 +31,6 @@ class VacanciesViewSet(viewsets.ModelViewSet):
             location_obj = Location(**data['location'])
             location_obj.save()
         data['location'] = location_obj
-
-        # канал нельзя создать POST-ом, поэтому при отсутсвии канала в БД поднимается Exception
-        # С ошибкой 500. Нужен try/except с raise Channel.DoesNotExists
-        channel_obj = Channel.objects.filter(**data['channel_id']).first()
-        data['channel_id'] = channel_obj
 
         # распаковываем и создаём технологии при отсутствии их в БД
         old_tech = data.pop('technologies')
