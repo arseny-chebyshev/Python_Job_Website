a944aaf2abaca55eddc2ccbacb144f9153e30ea8
arseny-chebyshev <arseny.chebyshev@gmail.com>
2022-06-13 01:48:39 +0500

    API GET-request Filtering and Validation

---
+++ django/api/utils.py
@@ -0,0 +1,32 @@
+from typing import List, Dict
+from django.db.models import Model
+from rest_framework.exceptions import ValidationError
+
+
+def get_object_or_raise_400(cls: Model, **kwargs):
+    obj = cls.objects.filter(**kwargs).first()
+    if not obj:
+        available = [dct for dct in cls.objects.all().values()]
+        json_msg = {"error": f"В таблице {cls.__name__} нет данных со следующими параметрами: {kwargs}.",
+                    "available": available}
+        raise ValidationError(detail=json_msg)
+    return obj
+
+
+def get_object_or_create_new(cls: Model, **kwargs):
+    obj = cls.objects.filter(**kwargs).first()
+    if not obj:
+        obj = cls(**kwargs)
+        obj.save()
+    return obj
+
+
+def get_m2m_objects_or_create_new(cls: Model, obj_list: List[Dict]):
+    new_list = []
+    for dct in obj_list:
+        obj = cls.objects.filter(**dct).first()
+        if not obj:
+            obj = cls(**dct)
+            obj.save()
+        new_list.append(obj)
+    return new_list
--- django/api/views.py
+++ django/api/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponseBadRequest
 from rest_framework import viewsets
+from rest_framework.exceptions import ParseError
 from rest_framework.response import Response
 from vacancies.models import Vacancy, Technology, Role, Location, Channel
 from .serializers import VacancySerializer, TechnologySerializer
+from .utils import get_object_or_create_new, get_object_or_raise_400, get_m2m_objects_or_create_new
 
 
 class VacanciesViewSet(viewsets.ModelViewSet):
@@ -12,12 +14,12 @@ class VacanciesViewSet(viewsets.ModelViewSet):
         params = self.request.query_params.dict()
         # если есть параметры запроса:
         if params:
-            # проверка на допустимые параметры запроса, пока не работает
+            # проверка на допустимые параметры запроса
             valid_param_keys = {f.name for f in Vacancy._meta.get_fields()}
             param_keys = set(params)
             if not param_keys.issubset(valid_param_keys):
                 invalid_params = '\n'.join(param_keys - valid_param_keys)
-                return HttpResponseBadRequest(f"Данные параметры запроса недопустимы:\n{invalid_params}")
+                raise ParseError(detail=f"Данные параметры GET-запроса недопустимы: {invalid_params}")
             # чистим true/false до питоновских True/False
             params = {key: value.capitalize() if (value == 'true' or value == 'false') else value
                       for key, value in params.items()}
@@ -54,39 +56,13 @@ class VacanciesViewSet(viewsets.ModelViewSet):
     def create(self, request, *args, **kwargs):
         data = request.data
 
-        # Если роли из запроса не в БД, возвращаем 400 BAD REQUEST со списком доступных
-        role_obj = Role.objects.filter(**data['role']).first()
-        if not role_obj:
-            roles = '\n'.join([str(role_dct) for role_dct in Role.objects.all().values()])
-            return HttpResponseBadRequest(f'Данной "role" нет в базе данных. Попробуйте следующие:\n'
-                                          f'{roles}')
-        data['role'] = role_obj
+        data['role'] = get_object_or_raise_400(Role, **data['role'])
+        data['channel_id'] = get_object_or_raise_400(Channel, **data['channel_id'])
+        data['location'] = get_object_or_create_new(Location, **data['location'])
 
-        # Если канала из запроса нет в БД, возвращаем 400 BAD REQUEST со списком доступных
-        channel_obj = Channel.objects.filter(**data['channel_id']).first()
-        if not channel_obj:
-            channels = '\n'.join([str(ch_dct) for ch_dct in Channel.objects.all().values()])
-            return HttpResponseBadRequest(f'Данного "channel_id" нет в базе данных. Попробуйте следующие:\n'
-                                          f'{channels}')
-        data['channel_id'] = channel_obj
+        # распаковываем и создаём технологии при отсутствии их в БД, удаляем невалидные из data
+        new_tech = get_m2m_objects_or_create_new(Technology, data.pop('technologies'))
 
-        # ищем локацию в БД, если её нет - создаём
-        if 'location' in data.keys():
-            location_obj = Location.objects.filter(**data['location']).first()
-            if not location_obj:
-                location_obj = Location(**data['location'])
-                location_obj.save()
-            data['location'] = location_obj
-
-        # распаковываем и создаём технологии при отсутствии их в БД
-        old_tech = data.pop('technologies')
-        new_tech = []
-        for technology in old_tech:
-            tech_obj = Technology.objects.filter(**technology).first()
-            if not tech_obj:
-                tech_obj = Technology(**technology)
-                tech_obj.save()
-            new_tech.append(tech_obj)
         # создаём instance Вакансии, сохраняем в БД и добавляем ManytoMany технологии
         new_vacancy = Vacancy.objects.create(**data)
         new_vacancy.save()
