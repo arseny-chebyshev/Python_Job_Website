9daf07af932676c9bec4ec810b871a8e81dcc4f1
arseny-chebyshev <arseny.chebyshev@gmail.com>
2022-06-12 18:49:41 +0500

    VacanciesViewSet Filter development

--- django/api/views.py
+++ django/api/views.py
@@ -9,43 +9,50 @@ class VacanciesViewSet(viewsets.ModelViewSet):
     serializer_class = VacancySerializer
 
     def get_queryset(self):
-        vacancy = Vacancy.objects.all()
+        params = self.request.query_params.dict()
+        # если есть параметры запроса:
+        if params:
+            # проверка на допустимые параметры запроса, пока не работает
+            valid_param_keys = {f.name for f in Vacancy._meta.get_fields()}
+            param_keys = set(params)
+            if not param_keys.issubset(valid_param_keys):
+                invalid_params = '\n'.join(param_keys - valid_param_keys)
+                return HttpResponseBadRequest(f"Данные параметры запроса недопустимы:\n{invalid_params}")
+            # чистим true/false до питоновских True/False
+            params = {key: value.capitalize() if (value == 'true' or value == 'false') else value
+                      for key, value in params.items()}
+            # смотрим, нет ли nested полей в запросе:
+            if not any([key in params for key in ('role', 'location', 'channel_id', 'technologies')]):
+                # если их нет - стандартно фильтруем по всем параметрам.
+                queryset = Vacancy.objects.filter(**params)
+            else:
+                cleaned_params = {}
+                for key, value in params.items():
+                    # Специально исключаем FK и Many-to-Many из этого фильтра, так как при простой
+                    # распаковке они не передадутся по названиям
+                    if key == 'technologies':
+                        tech_list = value.split(', ')
+                    elif key == 'location':
+                        value = value.capitalize()
+                        cleaned_params[key] = Location.objects.filter(name=value).first().id
+                    elif key == 'channel_id':
+                        cleaned_params[key] = Channel.objects.filter(name=value).first().id
+                    else:
+                        cleaned_params[key] = value
+                queryset = Vacancy.objects.filter(**cleaned_params)
+                # Фильтруем по каждой вакансии отдельно
+                if 'technologies' in params.keys():
+                    for tech in tech_list:
+                        tech_obj = Technology.objects.get(name=tech)
+                        queryset = queryset.filter(technologies=tech_obj)
 
-        # Собираем все query-параметры для дальнейшей фильтрации
-        params = self.request.query_params
-        cleaned_params = {}
-
-        # Переводим данные из QueryDict в понятный для фильтрации формат
-        if len(params.dict()) > 0:
-            for key, value in params.items():
-                value = ''.join(value)
-                if value == 'true':
-                    value = True
-                elif value == 'false':
-                    value = False
-                # Специально исключаем технологии из этого фильтра, так как при простой
-                # распаковке они не передадутся по названиям
-                if key == 'technologies':
-                    tech_list = value.split()
-                else:
-                    cleaned_params[key] = value
-
-            vacancy = vacancy.filter(**cleaned_params)
-            # Фильтруем по каждой вакансии отдельно
-            if len(tech_list) > 0:
-                for tech in tech_list:
-                    tech_obj = Technology.objects.get(name=tech)
-                    vacancy = vacancy.filter(technologies=tech_obj)
-
-        # На данный момент модуль не обрабатывает ситуаций, когда в поисковых параметрах
-        # появился какой-то мусор (условно ?ahaha=ahaha). Подразумевается, что единственные,
-        # кто будет взаимодействовать с API - это фронтэнд и бот, так что в теории не так 
-        # критично, но возможно проверка была бы полезна
-        return vacancy
+        # если нет параметров запроса:
+        else:
+            queryset = Vacancy.objects.all()
+        return queryset
 
     def create(self, request, *args, **kwargs):
         data = request.data
-
 
         # Если роли из запроса не в БД, возвращаем 400 BAD REQUEST со списком доступных
         role_obj = Role.objects.filter(**data['role']).first()
@@ -54,7 +61,6 @@ class VacanciesViewSet(viewsets.ModelViewSet):
             return HttpResponseBadRequest(f'Данной "role" нет в базе данных. Попробуйте следующие:\n'
                                           f'{roles}')
         data['role'] = role_obj
-
 
         # Если канала из запроса нет в БД, возвращаем 400 BAD REQUEST со списком доступных
         channel_obj = Channel.objects.filter(**data['channel_id']).first()
