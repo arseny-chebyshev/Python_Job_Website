18b0ba3a80454f941e651ffb73add3eaf38f5160
arseny-chebyshev <arseny.chebyshev@gmail.com>
2022-06-15 01:39:55 +0500

    Update test db, change django.api.serializers.VacancySerializer.to_representation and e xclude

--- django/api/serializers.py
+++ django/api/serializers.py
@@ -18,11 +18,19 @@ class VacancySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Vacancy
-        fields = '__all__'
+        # fields = '__all__'
+        exclude = ('min_salary_currency', 'max_salary_currency')
         depth = 1
 
     # более гибкая настройка репрезентации полей, можно поставить любое значение
     def to_representation(self, instance):
         rep = super(VacancySerializer, self).to_representation(instance)
         # здесь можно прописать красивые атрибуты через rep['имя поля'] = "красивый атрибут"
+        # на какое-то время для интеграции с фронтом делаем поля не вложенными при представлении
+        rep['min_salary'] = f"{instance.min_salary.amount}, {instance.min_salary.currency}"
+        rep['max_salary'] = f"{instance.max_salary.amount}, {instance.max_salary.currency}"
+        rep['role'] = instance.role.name
+        rep['location'] = instance.location.name
+        rep['channel_id'] = instance.channel_id.url
+        rep['technologies'] = [tech.name for tech in instance.technologies.all()]
         return rep
