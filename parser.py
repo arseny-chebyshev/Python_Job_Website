515bd3dee464da012dd468eaf788adaf026bd094
unknown <k.nikita@ortel.su>
2022-06-16 13:43:26 +0500

    Сделал правки по коду. Вилку заплат. Коррект локации. Добавил технологию. Внес коррективы по удаленке или офис.

--- parser.py
+++ parser.py
@@ -1,25 +1,40 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 
-last_message_id = 1516 # номер последжней записи
-vacancy = []
+last_message_id = 1516
+result = []
 
-for msg in range(1513, 1517):
+def salary_in(value):
+    result = []
+    val = re.findall(r'[\d]+', value)
+    if len(val) == 2:
+        result = ' '.join(val)
+    elif len(val) == 4:
+        after = ' '.join([val[0], val[1]])
+        before = ' '.join([val[2], val[3]])
+        result = '-'.join([after, before])
+    return result
+
+for msg in range(1520, 1526):
     vacancy = {'role': 0, 'salary': 0, 'mode': 0, 'location': 0, 'technology': 0}
     salary = 0
     location = []
-    page = requests.get(f'https://t.me/job_python/{msg}').content.decode() # получаем информацию о вакансии
-    # print(page)
+    page = requests.get(f'https://t.me/job_python/{msg}').content.decode()
     soup = BeautifulSoup(page, features="html.parser")
-    title = soup.find('meta', property="og:description")['content'].split('\n') # требуемый текст вакансии
-        if '#удаленка' in title[1] or '#гибрид' in title[1] or '#офис' in title[1] or '#полныйдень' in title[1]:
+    title = soup.find('meta', property="og:description")['content'].split('\n')
+    if '#удаленка' in title[1] or '#гибрид' in title[1] or '#офис' in title[1] or '#полныйдень' in title[1]:
         vacancy['role'] = str(title[0]) # добавляем название вакансии
         if title[2][-1] == '₽':
-            salary = title[2].isdigit()
+            salary = salary_in(title[2])
         vacancy['salary'] = salary # добавляем зарплату
-        vacancy['mode'] = title[1] # добавляем формат
-        vacancy['location'] = title[3] # добавляем локацию
+        if 'удаленка' in title[1]:
+            vacancy['mode'] = '#удаленка' # добавляем формат
+        else:
+            vacancy['mode'] = '#офис'
+        vacancy['location'] = title[3].replace(':','') # добавляем локацию
+        vacancy['technology'] = title[-1]
         result.append(vacancy)
 
-# for value in vacancy:
-#     print(value[0])
+for value in result:
+    print(value)
