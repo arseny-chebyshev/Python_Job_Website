122092f3ff62eb908ab926e7bd84fa81477e787c
arseny-chebyshev <arseny.chebyshev@gmail.com>
2022-06-18 20:32:54 +0500

    Pagination for API

--- django/api/views.py
+++ django/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.exceptions import ParseError
 from rest_framework.response import Response
 from vacancies.models import Vacancy, Technology, Role, Location, Channel
@@ -6,19 +7,33 @@ from .serializers import VacancySerializer, TechnologySerializer
 from .utils import get_object_or_create_new, get_object_or_raise_400, get_m2m_objects_or_create_new
 
 
+class VacanciesPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'limit'
+
+
 class VacanciesViewSet(viewsets.ModelViewSet):
     serializer_class = VacancySerializer
+    pagination_class = VacanciesPagination
 
     def get_queryset(self):
         params = self.request.query_params.dict()
         # если есть параметры запроса:
         if params:
+            pagination_params = {'page': None, 'limit': None}
+            for key in pagination_params:
+                if key in params:
+                    params.pop(key)
+
             # проверка на допустимые параметры запроса
             valid_param_keys = {f.name for f in Vacancy._meta.get_fields()}
+            valid_param_keys.update(pagination_params.keys())
             param_keys = set(params)
             if not param_keys.issubset(valid_param_keys):
                 invalid_params = '\n'.join(param_keys - valid_param_keys)
-                raise ParseError(detail=f"Данные параметры GET-запроса недопустимы: {invalid_params}")
+                raise ParseError(detail=f"Данные параметры GET-запроса недопустимы: {invalid_params}."
+                                        f"Попробуйте следующие: {valid_param_keys}")
+
             # чистим true/false до питоновских True/False
             params = {key: value.capitalize() if (value == 'true' or value == 'false') else value
                       for key, value in params.items()}
@@ -33,6 +48,8 @@ class VacanciesViewSet(viewsets.ModelViewSet):
                     # распаковке они не передадутся по названиям
                     if key == 'technologies':
                         tech_list = value.split(', ')
+                    elif key == 'role':
+                        cleaned_params[key] = Role.objects.filter(name=value).first().id
                     elif key == 'location':
                         value = value.capitalize()
                         cleaned_params[key] = Location.objects.filter(name=value).first().id
