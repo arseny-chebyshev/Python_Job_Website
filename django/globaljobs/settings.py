57307c5c1612bdc98fddcaf90671af85920506f6
arseny-chebyshev <arseny.chebyshev@gmail.com>
2022-07-19 02:04:19 +0500

    Добавление зависимостей sass для npm и debug=False

--- django/globaljobs/settings.py
+++ django/globaljobs/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # в Docker network контейнеры связываются друг с другом по названиям вместо IP
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'django', 'parser'] 
 
